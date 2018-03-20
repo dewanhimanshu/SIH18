09cc064561be3d0d4d280183d286a9d2167e046e
SHLOK WALIA <virgoshlok@gmail.com>
2018-03-20 23:57:51 +0530

    added additional fields to devise and basic user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :permit_params, if: :devise_controller?
+
+  protected
+  def permit_params
+
+    devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:first_name, :last_name, :birthday, :email, :password, :remember_me)}
+    devise_parameter_sanitizer.permit(:sign_in){|u| u.permit(:email, :password, :remember_me)}
+    devise_parameter_sanitizer.permit(:account_update){|u| u.permit(:first_name, :last_name, :birthday, :email, :password, :current_password, :remember_me)}
+  end
 end
