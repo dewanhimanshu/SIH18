ef7de76eee526221d401b7ec65d9cfb727d0343f
SHLOK WALIA <virgoshlok@gmail.com>
2018-03-21 20:48:53 +0530

    added feature to accept or decline application

--- db/migrate/20180321063559_add_feedback_to_apply.rb
+++ db/migrate/20180321063559_add_feedback_to_apply.rb
@@ -1,5 +1,5 @@
 class AddFeedbackToApply < ActiveRecord::Migration[5.1]
   def change
-    add_column :applies, :feedback, :text
+    add_column :applies, :feedback, :text, null: false, default: ""
   end
 end
