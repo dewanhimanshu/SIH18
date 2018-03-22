f8b7e3a2369c030af00477b76ce4016dc97bf2d8
dewanhimanshu <himanshudewan786@gmail.com>
2018-03-23 01:18:42 +0530

    fixed mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable ,:confirmable, :lockable, :timeoutable
-
+         :recoverable, :rememberable, :trackable, :validatable 
   has_one :apply
 
 
@@ -16,7 +15,7 @@ class User < ApplicationRecord
     end
 
     def welcome_email
-     Usermailer.welcome_email(self).deliver
+     UserMailer.welcome_email(self).deliver
     end
 
 end
