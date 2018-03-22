56f574ec7e3316ed74f5c2c09b1b4b71de57b6f0
dewanhimanshu <himanshudewan786@gmail.com>
2018-03-22 18:56:54 +0530

    paperboat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322130615) do
+ActiveRecord::Schema.define(version: 20180322131728) do
 
   create_table "applies", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20180322130615) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.text "feedback", default: "", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
